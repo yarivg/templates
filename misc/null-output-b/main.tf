05d94cc600dd71c6c1e65d71801a794c8b3a901f
yarivg <yarivgavriel2@gmail.com>
2024-10-01 10:23:33 +0300

    add the vars to outputs

--- misc/null-output-b/main.tf
+++ misc/null-output-b/main.tf
@@ -4,3 +4,7 @@ resource "null_resource" "null1" {
 output "b" {
   value = "b as an output"
 }
+
+output "b_required_var" {
+  value = var.b_required_var
+}
--- misc/null-output-c/main.tf
+++ misc/null-output-c/main.tf
@@ -4,3 +4,7 @@ resource "null_resource" "null1" {
 output "c" {
   value = "c as an output"
 }
+
+output "c_required_var" {
+  value = var.c_required_var
+}
