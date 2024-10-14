357dd0074659caef293b326641a970c529aa4294
yarivg <yarivgavriel2@gmail.com>
2024-10-14 14:13:52 +0300

    add more outputs

--- misc/null-with-sensitive-value/main.tf
+++ misc/null-with-sensitive-value/main.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "null1" {
 }
 
-output "a" {
+output "sensitive_obj" {
   value = {
     endpoint = "<http://core.cluster-c6e7cyvkajcg.eu-west-2.rds.amazonaws.com|core.cluster-c6e7cyvkajcg.eu-west-2.rds.amazonaws.com>"
     reader_endpoint = "<http://core.cluster-c6e7cyvkajcg.eu-west-2.rds.amazonaws.com|core.cluster-c6e7cyvkajcg.eu-west-2.rds.amazonaws.com>"
@@ -9,10 +9,58 @@ output "a" {
       username = "gitlab"
       fake_password = "foo"
     }
-    flyway = {
-      username = "flyway"
-      fake_password = "bar"
-    }
   }
   sensitive = true
+}
+
+output "sensitive_str" {
+  value = "sensitive string"
+  sensitive = true
+}
+
+output "sensitive_list" {
+  value = ["sensitive", "list"]
+  sensitive = true
+}
+
+output "sensitive_map" {
+  value = {
+    key = "value"
+  }
+  sensitive = true
+}
+
+output "sensitive_number" {
+  value = 42
+  sensitive = true
+}
+
+// non sensitive
+output "non_sensitive_obj" {
+  value = {
+    endpoint = "<http://core.cluster-c6e7cyvkajcg.eu-west-2.rds.amazonaws.com|core.cluster-c6e7cyvkajcg.eu-west-2.rds.amazonaws.com>"
+    reader_endpoint = "<http://core.cluster-c6e7cyvkajcg.eu-west-2.rds.amazonaws.com|core.cluster-c6e7cyvkajcg.eu-west-2.rds.amazonaws.com>"
+    service = {
+      username = "gitlab"
+      fake_password = "foo"
+    }
+  }
+}
+
+output "non_sensitive_str" {
+  value = "non sensitive string"
+}
+
+output "non_sensitive_list" {
+  value = ["non sensitive", "list"]
+}
+
+output "non_sensitive_map" {
+  value = {
+    key = "value"
+  }
+}
+
+output "non_sensitive_number" {
+  value = 42
 }
