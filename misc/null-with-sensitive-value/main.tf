f2ea6df7b631903f69be180800438485a785b342
yarivg <yarivgavriel2@gmail.com>
2024-10-14 11:48:45 +0300

    change output to original value that causes the error

--- misc/null-with-sensitive-value/main.tf
+++ misc/null-with-sensitive-value/main.tf
@@ -4,6 +4,15 @@ resource "null_resource" "null1" {
 output "a" {
   value = {
     endpoint = "<http://core.cluster-c6e7cyvkajcg.eu-west-2.rds.amazonaws.com|core.cluster-c6e7cyvkajcg.eu-west-2.rds.amazonaws.com>"
+    reader_endpoint = "<http://core.cluster-c6e7cyvkajcg.eu-west-2.rds.amazonaws.com|core.cluster-c6e7cyvkajcg.eu-west-2.rds.amazonaws.com>"
+    service = {
+      username = "gitlab"
+      fake_password = "foo"
+    }
+    flyway = {
+      username = "flyway"
+      fake_password = "bar"
+    }
   }
   sensitive = true
 }
