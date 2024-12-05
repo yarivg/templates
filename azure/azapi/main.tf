010a4f8c0e1410bb8bf5b4958b917ace0e5e2ec6
yarivg <yarivgavriel2@gmail.com>
2024-12-05 15:20:50 +0200

    add azurerm

--- azure/azapi/main.tf
+++ azure/azapi/main.tf
@@ -1,9 +1,19 @@
 terraform {
   required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.74" # Specify the version you want
+    }
     azapi = {
-      source = "azure/azapi"
+      source  = "azure/azapi"
+      version = "~> 1.8" # Specify the version you want
     }
   }
+}
+
+
+provider "azurerm" {
+  features {}
 }
 
 provider "azapi" {
