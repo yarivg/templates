9e34b23704807159efe14facd70652a4326e90a7
yarivg <yarivgavriel2@gmail.com>
2024-12-05 14:31:06 +0200

    add azapi

---
+++ azure/azapi/main.tf
@@ -0,0 +1,26 @@
+provider "azapi" {}
+
+resource "azapi_resource" "example_resource_group" {
+  type     = "Microsoft.Resources/resourceGroups@2022-09-01" # Resource Group ARM API version
+  name     = "example-resource-group"
+  location = "East US"
+
+  body = jsonencode({
+    properties = {} # Resource groups do not have additional properties
+  })
+}
+
+resource "azapi_resource" "example_storage_account" {
+  type      = "Microsoft.Storage/storageAccounts@2022-09-01" # Storage Account ARM API version
+  name      = "examplestorageacct"
+  location  = azapi_resource.example_resource_group.location
+  parent_id = azapi_resource.example_resource_group.id
+
+  body = jsonencode({
+    sku = {
+      name = "Standard_LRS"
+    }
+    kind     = "StorageV2"
+    properties = {}
+  })
+}
