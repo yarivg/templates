3c0cf3e558cb233c9d0b9c2cc49d788b4d155516
yarivg <yarivgavriel2@gmail.com>
2025-04-14 17:17:32 +0300

    add project_var test using the env0 provider

---
+++ env0-provider/project_var/conf.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    env0 = {
+      version = "1.25.0"
+      source  = "env0/env0"
+    }
+  }
+}
+
+variable "env0_api_key" {
+  default = "***"
+}
+variable "env0_api_secret" {
+  default = "***"
+}
+
+variable "TEAM_ID" {
+  default = "***"
+}
+
+variable "ROLE_ID" {
+  default = "***"
+}
+
+#output "some-output" {
+#  value = "asd"
+#}
+
+#output "some-output-2" {
+#  value = var.env0_api_key
+#}
+#
+#output "some-output-3" {
+#  value = "asd ${var.env0_api_key}"
+#}
+
+provider "env0" {
+  api_key      = var.env0_api_key
+  api_secret   = var.env0_api_secret
+  api_endpoint = "https://api-dev.dev.env0.com"
+}
---
+++ env0-provider/project_var/main.tf
@@ -0,0 +1,30 @@
+data "env0_organization" "my_organization" {}
+
+output "organization_name" {
+  value = data.env0_organization.my_organization.name
+}
+
+resource "env0_project" "project_with_cd" {
+  name = "test_project_with_var_visa"
+}
+
+resource "env0_project_policy" "example" {
+  project_id = env0_project.project_with_cd.id
+
+  continuous_deployment_default = false
+  run_pull_request_plan_default = true
+}
+
+resource "env0_configuration_variable" "project_var" {
+  name       = "project_metadata"
+  type       = "terraform"
+  value      = "{}"
+  project_id = env0_project.project_with_cd.id
+
+  is_read_only = false
+  is_sensitive = false
+  format       = "JSON"
+
+  depends_on = [env0_project.project_with_cd]
+}
+
