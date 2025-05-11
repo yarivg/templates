2fb64e6e1c4cc2ac3de48c3f51150013bab7be95
yarivg <yarivgavriel2@gmail.com>
2025-05-11 14:19:31 +0300

    create iam member

--- gcp/service-account-key/main.tf
+++ gcp/service-account-key/main.tf
@@ -4,7 +4,7 @@ terraform {
 
 provider "google" {
   version = ">=3.0.0"
-  project = var.project_name
+  project = var.project_id
 }
 
 ## Data Resources
@@ -19,16 +19,23 @@ resource "google_service_account" "account" {
   display_name = var.account_display_name
 }
 
-resource "google_service_account_key" "key" {
-  service_account_id = google_service_account.account.name
-  public_key_type    = "TYPE_X509_PEM_FILE"
-}
+#resource "google_service_account_key" "key" {
+#  service_account_id = google_service_account.account.name
+#  public_key_type    = "TYPE_X509_PEM_FILE"
+#}
+#
+#resource "google_storage_bucket" "storage_buckets" {
+#  count    = 2
+#  name     = "yariv-demo-buckets-${count.index}-${var.project_name}"
+#  location = "US"
+#  project = var.project_id
+#}
 
-resource "google_storage_bucket" "storage_buckets" {
-  count    = 2
-  name     = "yariv-demo-buckets-${count.index}-${var.project_name}"
-  location = "US"
-  project = var.project_id
+resource "google_project_iam_member" "viewer" {
+  count   = 2
+  project = "your-project-id"
+  role    = "roles/viewer"
+  member  = "serviceAccount:asd${count.index}@gmail.com"
 }
 
 ## Modules
