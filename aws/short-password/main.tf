179d58001cb1a25ffe2884201b20d95221ee3c09
yarivg <yarivgavriel2@gmail.com>
2025-09-15 16:24:57 +0300

    add short password

---
+++ aws/short-password/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  cloud {
+    hostname     = "backend-dev.api.dev.env0.com"
+    organization = "bd12a04a-b051-4f51-9a8a-8a32ac198802.3cb78079-de2c-4aa8-b7e2-7eb0f1c797c8"
+    workspaces {
+      name = "short-password"
+    }
+  }
+  required_version = "1.5.7"
+}
+
+resource "aws_iam_account_password_policy" "fail_insecure_password" {
+  minimum_password_length        = 8  # Too short (below required 14)
+  require_lowercase_characters   = false  # Missing requirement
+  require_uppercase_characters   = false  # Missing requirement
+  require_numbers                = false  # Missing requirement
+  require_symbols                = false  # Missing requirement
+  allow_users_to_change_password = false  # Missing requirement
+  max_password_age               = 365    # Too long (above allowed 90)
+  password_reuse_prevention      = 1      # Too low (below required 5)
+  hard_expiry                    = false
+}
