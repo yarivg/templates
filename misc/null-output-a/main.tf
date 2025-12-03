4e06fe932de42e234b7fd51db3f26c7536326bfd
Yariv Gavriel <yarivgavriel2@gmail.com>
2025-12-03 13:33:17 +0200

    Update main.tf

--- misc/null-output-a/main.tf
+++ misc/null-output-a/main.tf
@@ -1,6 +1,18 @@
 resource "null_resource" "null1" {
 }
 
+variable "dns" {
+  type = object({
+    ...
+  })
+
+  default = null
+}
+
+output "out_dns" {
+  value = var.dns == null ? "null DNS" : "Existing DNS" + tostring(var.dns)
+}
+
 output "a" {
   value = "a as an output"
 }
